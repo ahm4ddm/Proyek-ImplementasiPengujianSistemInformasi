2becea15e4912ec6100467e297aa74b8516e29c5
ahm4ddm <dania9015@gmail.com>
2021-10-27 18:19:56 +0700

    update route register

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,7 +50,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 $route['default_controller'] = 'main';
-$route['login'] = 'AuthCon';
-$route['register'] = 'RegistCon';
+$route['login'] = 'AuthCon/login';
+$route['register'] = 'AuthCon/register';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
