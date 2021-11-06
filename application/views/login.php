383e9f043bade99213896b899f30c820a3bf92a5
Ilham Ramadhan <76545550+aringojuice@users.noreply.github.com>
2021-11-06 21:25:59 +0700

    6 November 2021
    - add bg
    - add some modal function
    - fix some ui
    - change ui to white
    - remove base_url to make it auto

---
+++ application/views/login.php
@@ -0,0 +1,201 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+?>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');
+
+* {
+  font-family: 'Poppins', sans-serif;
+}
+
+.modal-header {
+    border-bottom: 0 none;
+}
+
+.modal-footer {
+    border-top: 0 none;
+}
+
+#menuDropdown:focus {
+  outline: none !important;
+  box-shadow:none;
+}
+</style>
+<!-- <body class="bg-dark"> -->
+
+  <!-- Button trigger modal -->
+  <!-- <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#leaderboardModal">
+    Leaderboard
+  </button>
+  <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#registerModal">
+    Register
+  </button>
+  <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#profileModal">
+    Profile
+  </button> -->
+
+  <!-- Leaderboard Modal -->
+  <div class="modal fade" id="leaderboardModal" tabindex="-1" aria-labelledby="profileModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="registerModalLabel">Leaderboard</h5>
+          <button type="button" class="btn-close bg-white" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body" style="padding-left:30px;padding-right:30px">
+          <form>
+            <div class="row" style="padding-bottom:20px">
+              <div class="col-md-1">
+                <p>1</p>
+              </div>
+              <div class="col-md-3">
+                <img src="<?php echo base_url()?>/assets/img/profile.jpeg" width="75px" height="75px" style="border-radius:50%;object-fit: cover;" alt="profile picture">
+              </div>
+              <div class="col-md-5">
+                <p>Lorem</p>
+              </div>
+              <div class="col-md-3">
+                <p>1050</p>
+              </div>
+            </div>
+            <div class="row" style="padding-bottom:20px">
+              <div class="col-md-1">
+                <p>2</p>
+              </div>
+              <div class="col-md-3">
+                <img src="<?php echo base_url()?>/assets/img/profile.jpeg" width="75px" height="75px" style="border-radius:50%;object-fit: cover;" alt="profile picture">
+              </div>
+              <div class="col-md-5">
+                <p>Ipsum</p>
+              </div>
+              <div class="col-md-3">
+                <p>1000</p>
+              </div>
+            </div>
+        </form>
+      </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Login Modal -->
+<div class="modal fade" id="loginModal" tabindex="-1" aria-labelledby="loginModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="registerModalLabel">Login</h5>
+          <button type="button" class="btn-close bg-white" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+      <form>
+        <div class="mb-3">
+          <input type="text" placeholder="Username" class="form-control text-center" id="usernameRegister" aria-describedby="email">
+        </div>
+        <div class="mb-3">
+          <input type="password" placeholder="Password" class="form-control text-center" id="passwordRegister">
+        </div>
+        <div class="mb-3 d-grid gap-2">
+          <button type="button" placeholder="Username" class="btn btn-block btn-secondary">Register</button>
+        </div>
+        <div class="mb-3 text-center">
+          <span>Don't have account yet? </span><a class="" data-bs-toggle="modal" style="color:black" data-bs-target="#registerModal"><strong>Register</strong></a>
+        </div>
+      </form>
+        </div>
+      </div>
+    </div>
+  </div>
+
+<!-- Register Modal -->
+<div class="modal fade" id="registerModal" tabindex="-1" aria-labelledby="registerModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="registerModalLabel">Register</h5>
+          <button type="button" class="btn-close bg-white" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+      <form>
+        <div class="mb-3">
+          <input type="email" placeholder="Email" class="form-control text-center" id="emailRegister" aria-describedby="email">
+        </div>
+        <div class="mb-3">
+          <input type="text" placeholder="Username" class="form-control text-center" id="usernameRegister" aria-describedby="username">
+        </div>
+        <div class="mb-3">
+          <input type="password" placeholder="Password" class="form-control text-center" id="passwordRegister">
+        </div>
+        <div class="mb-3">
+          <input type="password" placeholder="Confirm Password" class="form-control text-center" id="passwordConfirmationRegister">
+        </div>
+        <div class="mb-3 d-grid gap-2">
+          <button type="button" placeholder="Username" class="btn btn-block btn-secondary">Register</button>
+        </div>
+        <div class="mb-3 text-center">
+          <span>Already have an account? </span><a class="" style="color:black" data-bs-toggle="modal" data-bs-target="#loginModal"><strong>Login</strong></a>
+        </div>
+      </form>
+        </div>
+      </div>
+    </div>
+  </div>
+
+<!-- Profile Modal -->
+<div class="modal fade" id="profileModal" tabindex="-1" aria-labelledby="profileModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="btn-close bg-white" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body" style="padding-left:30px;padding-right:30px">
+            <div class="row" style="padding-bottom:20px">
+              <div class="col-md-3">
+                <img src="<?php echo base_url()?>/assets/img/profile.jpeg" width="100px" height="100px" style="border-radius:50%;object-fit: cover;" alt="profile picture">
+              </div>
+              <div class="col-md-9">
+                <h3>Lorem</h3>
+                <p>aaaaaaaaa</p>
+              </div>
+              </div>
+            <div class="row">
+              <div class="mb-3">
+                <button type="button" placeholder="Username" class="btn btn-outline-secondary" disabled>Grade A</button>
+            </div>
+        </form>
+      </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Profile Modal -->
+<div class="modal fade" id="leaderboardModal" tabindex="-1" aria-labelledby="leaderboardModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="btn-close bg-white" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body" style="padding-left:30px;padding-right:30px">
+            <div class="row" style="padding-bottom:20px">
+              <div class="col-md-3">
+                <img src="<?php echo base_url()?>/assets/img/profile.jpeg" width="100px" height="100px" style="border-radius:50%;object-fit: cover;" alt="profile picture">
+              </div>
+              <div class="col-md-9">
+                <h3>Lorem</h3>
+                <p>aaaaaaaaa</p>
+              </div>
+              </div>
+            <div class="row">
+              <div class="mb-3">
+                <button type="button" placeholder="Username" class="btn btn-outline-secondary" disabled>Grade A</button>
+            </div>
+        </form>
+      </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<script src="https://kit.fontawesome.com/ac17403586.js" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
--- application/views/main.php
+++ application/views/main.php
@@ -5,22 +5,66 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <style>
         body {
-        background-color: background-image: url("img/pexels-lumn-167699.jpg");
+
+        background:
+        /* top, transparent black, faked with gradient */ 
+        linear-gradient(
+          rgba(0, 0, 0, 0.3), 
+          rgba(0, 0, 0, 0.3)
+        ),
+        /* bottom, image */
+        url("<?php echo base_url()?>/assets/img/bg.jpg");
+        background-repeat: no-repeat;
+        background-size: cover;
+        }
+
+        .container-fluid {
+            padding-left:50px;
+            padding-right:50px;
         }
     </style>
     </head>
     <body>
         <div class="container-fluid">
-                <div>
-                    aaa
+
+                <div class="row">
+                    <!-- Dropdown -->
+                    <div class="dropdown col-md-10">
+                        <!-- Button Login -->
+                        <button type="button" class="btn btn-light" data-bs-toggle="modal" data-bs-target="#loginModal">
+                            Login
+                        </button>
+
+                        <button class="btn" type="button" id="menuDropdown" data-bs-toggle="dropdown" style="margin-left:-10px" aria-expanded="false">
+                        <h3 style="padding-top:5px" class="text-light"><img src="<?php echo base_url()?>/assets/img/profile.jpeg" width="30px" height="30px" style="border-radius:50%;object-fit: cover;" alt="profile picture">   
+                            Hi, Lorem</h3>
+                        </button>
+                        <ul class="dropdown-menu text-center" aria-labelledby="menuDropdown">
+                            <li><h3>Great!</h3><p>A</p></li>
+                            <li><a class="dropdown-item" data-bs-toggle="modal" data-bs-target="#profileModal">Profile</a></li>
+                            <li><a class="dropdown-item" >Activity</a></li>
+                            <li><a class="dropdown-item" >Logout</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-2">
+                        <div class="dropdown">
+                            <a class="btn btn-light dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
+                                Select Your Music
+                            </a>
+
+                            <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                                <li><a class="dropdown-item" href="#">Action</a></li>
+                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            </ul>
+                            <i style="font-size: 2rem;color:white;padding-left:10px;padding-top:10px" class="fas fa-music"></i>
+                        </div>
+                        
+                    </div>
                 </div>
                 <div class="col-md-12 d-flex flex-column h-75 justify-content-center align-items-center">
-                    <h1 style="font-size:10em">25:00</h1>
-                </div>
-                <div>
-                    bbb
+                    <h1 style="font-size:13em;letter-spacing: -10px;font-weight:800" class="text-light">25:00</h1>
                 </div>
         </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     </body>
 </html>
