ce132886608aeef45de0d4743ef824778bbc4664
Ilham Ramadhan <76545550+aringojuice@users.noreply.github.com>
2021-11-06 17:07:47 +0700

    Add Login and Register modal
    Add Login and Register modal

--- application/views/main.php
+++ application/views/main.php
@@ -3,6 +3,11 @@
     <title>Timer ganteng</title>
     <!-- <link rel="stylesheet" href="css/style.css"> -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <style>
+        body {
+        background-color: background-image: url("img/pexels-lumn-167699.jpg");
+        }
+    </style>
     </head>
     <body>
         <div class="container-fluid">
