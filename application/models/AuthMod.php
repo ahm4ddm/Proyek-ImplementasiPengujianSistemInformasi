e1b3a4910cf41eab7805702d92209fa8b70cc62d
ahm4ddm <dania9015@gmail.com>
2021-11-20 19:36:23 +0700

    change name input for username

--- application/models/AuthMod.php
+++ application/models/AuthMod.php
@@ -4,7 +4,7 @@ class AuthMod extends CI_Model
     function dbInsertUser()
     {
         $this->nama = htmlspecialchars($this->input->post('name_full', true));
-        $this->username = htmlspecialchars($this->input->post('user_name', true));
+        $this->username = htmlspecialchars($this->input->post('user_nameR', true));
         $this->email = htmlspecialchars($this->input->post('user_email', true));
         $this->password = password_hash($this->input->post('user_password1'), PASSWORD_ARGON2ID);
         $this->db->insert('users', $this);
