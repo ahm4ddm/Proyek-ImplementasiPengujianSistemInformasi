5b38101805a2776002da0c14dea6d4532ae8cb8f
ahm4ddm <dania9015@gmail.com>
2021-11-23 22:49:49 +0700

    FIXED POMODORO_MOD :heavy_check_mark:

--- application/models/PomodoroMod.php
+++ application/models/PomodoroMod.php
@@ -1,31 +1,8 @@
 <?php
 class PomodoroMod extends CI_Model
 {
-    public $waktu;
-    public $totalwaktu;
-
-    public function dbInitTime($data)
+    public function addTime($data)
     {
         return $this->db->insert('pomodoros', $data);
-    }
-
-    public function dbSelectTotalTime($id)
-    {
-        //$query = $this->db->select('totalwaktu')->get_where('pomodoros', ['id' => $id]);
-        $this->db->select('totalwaktu');
-        $query = $this->db->get_where('pomodoros', ['id' => $id])->row_array();
-        return $query;
-    }
-
-    public function dbUpdateTimer($data, $id)
-    {
-        return $this->db->update('pomodoros', $data, ['id' => $id]);
-    }
-
-    public function dbSelectTimeUser($id)
-    {
-        $this->db->select('tmpwaktu');
-        $query = $this->db->get_where('pomodoros', ['id' => $id])->result_array();
-        return $query;
     }
 }
